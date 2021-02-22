33ba1eb43ddcc5783900e39074edbd8783b0c1f7
RKHashmani <hashmani.raheem@metu.edu.tr>
2021-02-23 00:07:04 +0300

    Reduced Print Frequency

--- Open_Hackathon/SimpleMNIST/pytorch_CNN.py
+++ Open_Hackathon/SimpleMNIST/pytorch_CNN.py
@@ -84,7 +84,8 @@ correct = 0
 total = 0
 num_samples = 1000
 for i ,(images,labels) in enumerate(train_loader):
-    print ("Sample No:", i)
+    if i % 10 == 9:
+        print ("Sample No:", i)
     if i == num_samples:
         break
     images = images.to(device)
