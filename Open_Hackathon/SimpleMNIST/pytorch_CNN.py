f0582b0673fa482058c31fd5a4e1798bc253ba43
RKHashmani <hashmani.raheem@metu.edu.tr>
2021-02-22 22:40:37 +0300

    Limit Classes to 3, Add a Time Measurement

--- Open_Hackathon/SimpleMNIST/pytorch_CNN.py
+++ Open_Hackathon/SimpleMNIST/pytorch_CNN.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
+import time
 
 # Import the network/backbone to be used
 # from networks.backbone.SimpleNet import SimpleNet
@@ -29,6 +30,15 @@ test_data = datasets.MNIST(root = './data', train = False,
     transform = rand_transform, download = True)
 
 
+idx = (train_data.targets==0) | (train_data.targets==1) | (train_data.targets==2)
+train_data.targets = train_data.targets[idx]
+train_data.data = train_data.data[idx]
+
+idx = (test_data.targets==0) | (test_data.targets==1) | (test_data.targets==2)
+test_data.targets = test_data.targets[idx]
+test_data.data = test_data.data[idx]
+
+
 batch_size = 1
 train_loader = torch.utils.data.DataLoader(dataset = train_data, batch_size = batch_size, shuffle = True)
 test_loader = torch.utils.data.DataLoader(dataset =  test_data, batch_size = batch_size, shuffle = False)
@@ -44,6 +54,8 @@ optimizer = torch.optim.Adam( net.parameters(), lr=1.e-3)
 # Training
 num_epochs = 2
 num_iters_per_epoch = 10 # use only 5K iterations
+
+start_time = time.time()
 
 for epoch in range(num_epochs):
   for i ,(images,labels) in enumerate(train_loader):
@@ -63,6 +75,8 @@ for epoch in range(num_epochs):
         print('Epoch [%d/%d], Step [%d/%d], Loss: %.4f'
               % (epoch + 1, num_epochs, i + 1, num_iters_per_epoch, loss.item()))
 
+train_time = time.time()
+print("---Training took %s seconds ---" % (train_time - start_time))
 
 # Testing
 correct = 0
@@ -70,10 +84,12 @@ total = 0
 for images,labels in test_loader:
   images = images.to(device)
   labels = labels.to(device)
-  
+
   out = net(images)
   _, predicted_labels = torch.max(out,1)
   correct += (predicted_labels == labels).sum()
   total += labels.size(0)
 
-  print('Percent correct: %.3f'%((100.0*correct)/(total+1)))
+print('Percent correct: %.3f'%((100.0*correct)/(total+1)))
+
+print("---Testing took %s seconds ---" % (time.time() - train_time))
