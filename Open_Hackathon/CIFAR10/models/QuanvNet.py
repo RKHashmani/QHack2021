b22a8e49376e8f9b4b397a74d16cc13c517ee42e
RKHashmani <hashmani.raheem@metu.edu.tr>
2021-02-25 22:01:54 +0300

    Test 1 Quanv Layer

--- Open_Hackathon/CIFAR10/models/QuanvNet.py
+++ Open_Hackathon/CIFAR10/models/QuanvNet.py
@@ -15,8 +15,15 @@ class QuanvNet(nn.Module):
         self.conv4 = nn.Conv2d(64, 32, kernel_size=3, padding=1)
         # Same Max pool as above
         self.convPool = nn.Conv2d(32, 16, kernel_size=1)
+
+        # Classical Alts to Quanv Layers
         self.QuanvALT1 = nn.Conv2d(16, 8, kernel_size=3)
         self.QuanvALT2 = nn.Conv2d(8, 4, kernel_size=3)
+
+        # Quanv Layers
+        self.quanv1 = Quanv(kernal_size=3, output_depth=8)
+        self.quanv2 = Quanv(kernal_size=3, output_depth=4)
+
         # Flatten
         self.dropout = nn.Dropout(0.5)
         self.fc1 = nn.Linear(64, 32)
@@ -42,8 +49,14 @@ class QuanvNet(nn.Module):
         out = self.relu(self.conv4(out))
         out = self.pool(out)
         out = self.convPool(out)
+
+        # out = self.relu(self.QuanvALT1(out))
+        # out = self.relu(self.QuanvALT2(out))
+
+        # out = self.relu(self.quanv1(out))
         out = self.relu(self.QuanvALT1(out))
-        out = self.relu(self.QuanvALT2(out))
+        out = self.relu(self.quanv2(out))
+
         out = out.view(-1, 64)
         out = self.dropout(out)
         out = self.relu(self.fc1(out))
